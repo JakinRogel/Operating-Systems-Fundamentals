b9b39c1a0b8fd243095c763ec28ba96d5491d09a
Jakin Rogel <jakinrogel@gmail.com>
2024-06-05 15:59:49 -0700

    Implementation update

--- Topic 3/Deadlock Avoidance/avoidDeadlock.c
+++ Topic 3/Deadlock Avoidance/avoidDeadlock.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
 #include <pthread.h>
 #include <time.h>
 #include <errno.h>
@@ -10,6 +9,9 @@
 
 // Global variables
 pthread_mutex_t mutex;
+pthread_mutex_t log_mutex;
+pthread_cond_t cond;
+int resource_available = 1;
 FILE* log_file;
 
 // Structure to pass data to threads
@@ -17,37 +19,69 @@ typedef struct {
     int thread_id;
 } thread_data_t;
 
-// Signal handler for timeout
-void signalHandler(int signum) {
-    fprintf(log_file, "Thread %ld: Timed out. Releasing the file.\n", pthread_self());
-    pthread_mutex_unlock(&mutex); // Release the mutex
-    pthread_exit(NULL); // Exit the thread
-}
-
 // Thread function
 void* threadFunction(void* arg) {
     thread_data_t* data = (thread_data_t*) arg;
     int thread_id = data->thread_id;
+    int acquired = 0;
+
     fprintf(log_file, "\n\n\n           *** NEW LOG ***\n");
+
     for (int i = 0; i < 4; ++i) {
+        pthread_mutex_lock(&log_mutex);
         fprintf(log_file, "Thread %d: Trying to access the file...\n", thread_id);
+        pthread_mutex_unlock(&log_mutex);
 
-        signal(SIGALRM, signalHandler); // Set up signal handler for SIGALRM
+        pthread_mutex_lock(&mutex);
+        while (!resource_available) {
+            struct timespec ts;
+            clock_gettime(CLOCK_REALTIME, &ts);
+            ts.tv_sec += 3;
 
-        if (pthread_mutex_lock(&mutex) != 0) {
-            perror("pthread_mutex_lock");
-            pthread_exit(NULL);
+            int res = pthread_cond_timedwait(&cond, &mutex, &ts);
+            if (res == ETIMEDOUT) {
+                pthread_mutex_lock(&log_mutex);
+                fprintf(log_file, "Thread %d: Timed out. Releasing the file.\n", thread_id);
+                pthread_mutex_unlock(&log_mutex);
+                break;
+            }
         }
 
-        fprintf(log_file, "Thread %d: file acquired successfully!\n", thread_id);
-        alarm(3); // Set a timer of 3 seconds
-        int work_time = rand() % 6 + 1; // Generate random work time
-        fprintf(log_file, "Thread %d: Working for %d seconds...\n", thread_id, work_time);
-        sleep(work_time); // Simulate some work with the resource
-        pthread_mutex_unlock(&mutex); // Release the mutex
-        alarm(0); // Cancel the alarm
+        if (resource_available) {
+            resource_available = 0;
+            acquired = 1;
+        }
+        pthread_mutex_unlock(&mutex);
+
+        if (acquired) {
+            pthread_mutex_lock(&log_mutex);
+            fprintf(log_file, "Thread %d: file acquired successfully!\n", thread_id);
+            pthread_mutex_unlock(&log_mutex);
+
+            int work_time = rand() % 6 + 1; // Generate random work time
+            pthread_mutex_lock(&log_mutex);
+            fprintf(log_file, "Thread %d: Working for %d seconds...\n", thread_id, work_time);
+            pthread_mutex_unlock(&log_mutex);
+
+            sleep(work_time); // Simulate some work with the resource
+
+            pthread_mutex_lock(&mutex);
+            resource_available = 1;
+            pthread_cond_signal(&cond);
+            pthread_mutex_unlock(&mutex);
+
+            pthread_mutex_lock(&log_mutex);
+            fprintf(log_file, "Thread %d: Released the file.\n", thread_id);
+            pthread_mutex_unlock(&log_mutex);
+
+            acquired = 0; // Reset acquired flag
+        }
     }
+
+    pthread_mutex_lock(&log_mutex);
     fprintf(log_file, "Thread %d finished...\n", thread_id);
+    pthread_mutex_unlock(&log_mutex);
+
     pthread_exit(NULL);
 }
 
@@ -62,9 +96,19 @@ int main() {
     // Seed the random number generator
     srand(time(NULL));
 
-    // Initialize mutex
+    // Initialize mutex and condition variable
     if (pthread_mutex_init(&mutex, NULL) != 0) {
         perror("Mutex initialization failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_mutex_init(&log_mutex, NULL) != 0) {
+        perror("Log mutex initialization failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_cond_init(&cond, NULL) != 0) {
+        perror("Condition variable initialization failed");
         exit(EXIT_FAILURE);
     }
 
@@ -85,16 +129,33 @@ int main() {
     }
 
     // Clean up
+    pthread_mutex_lock(&log_mutex);
     fprintf(log_file, "Clean up started........\n");
-    // Destroy mutex
+    pthread_mutex_unlock(&log_mutex);
+
+    // Destroy mutexes and condition variable
     if (pthread_mutex_destroy(&mutex) != 0) {
         perror("Mutex destruction failed\n");
         exit(EXIT_FAILURE);
     }
+
+    if (pthread_mutex_destroy(&log_mutex) != 0) {
+        perror("Log mutex destruction failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_cond_destroy(&cond) != 0) {
+        perror("Condition variable destruction failed\n");
+        exit(EXIT_FAILURE);
+    }
+
     // Close log file
     fclose(log_file);
-    fprintf(log_file, "Clean up finished........\n");
 
+    pthread_mutex_lock(&log_mutex);
+    fprintf(log_file, "Clean up finished........\n");
     fprintf(log_file, "Program complete.\n");
+    pthread_mutex_unlock(&log_mutex);
+
     return 0;
 }
