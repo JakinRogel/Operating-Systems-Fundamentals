ca2b3c68e8fcb791c3d349cee675e23f8f868cc4
Jakin Rogel <jakinrogel@gmail.com>
2024-06-23 17:28:52 -0700

    Update

---
+++ Topic 5/Activity 5/scriptFive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "No search name provided"
+    exit 1
+fi
+
+search_name=$1
+sorted_names=$(sort names.txt)
+
+for name in $sorted_names; do
+    echo $name
+    if [ "$name" == "$search_name" ]; then
+        echo "Found $search_name"
+    fi
+done
---
+++ Topic 5/Activity 5/scriptOne.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+
+number=$1
+
+if [ $number -lt 50 ]; then
+    echo "The number is less than 50"
+elif [ $number -eq 50 ]; then
+    echo "The number is equal to 50"
+else
+    echo "The number is greater than 50"
+fi
